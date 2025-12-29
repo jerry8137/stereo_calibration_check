52cdd2ffb1cf2fbe1505c9350b2b1bdf238c44ca
Jerry Hou <jerry8137@gmail.com>
2025-12-29 11:32:21 -0500

    feat(utils): visualize image side by side

---
+++ utils/visualization.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import cv2 as cv
+import numpy as np
+
+
+def display_images_side_by_side(img1: np.ndarray,
+                                img2: np.ndarray,
+                                title1: str = "Image 1",
+                                title2: str = "Image 2") -> None:
+    """
+    Display two images side by side for comparison.
+    """
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(img1, cmap='gray')
+    plt.title(title1)
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(img2, cmap='gray')
+    plt.title(title2)
+    plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def save_images_side_by_side(img1: np.ndarray, img2: np.ndarray,
+                             output_path: str) -> None:
+    """
+    Save two images side by side to a single image file.
+    """
+    combined_image = np.hstack((img1, img2))
+    cv.imwrite(output_path, combined_image)
