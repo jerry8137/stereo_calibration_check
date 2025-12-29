fb903c9ae20f8c855777530785dbd0aa64c8e702
Jerry Hou <jerry8137@gmail.com>
2025-12-29 11:32:01 -0500

    feat(utils): load intrinsics file

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -2,6 +2,8 @@
 Utility functions for file operations.
 """
 import yaml
+import numpy as np
+
 
 def load_yaml(file_path: str) -> dict:
     """
@@ -10,4 +12,21 @@ def load_yaml(file_path: str) -> dict:
 
     with open(file_path, 'r') as file:
         data = yaml.safe_load(file)
+    return data
+
+
+def load_intrinsics(calib_path: str) -> dict:
+    """
+    Load camera intrinsics from a YAML file.
+    """
+    intrinsics = load_yaml(calib_path)
+    data = dict()
+    data['K'] = np.array(intrinsics['camera_matrix']['data']).reshape(3, 3)
+    data['D'] = np.array(intrinsics['distortion_coefficients']['data'])
+    data['R'] = np.array(intrinsics['rectification_matrix']['data']).reshape(
+        3, 3)
+    data['P'] = np.array(intrinsics['projection_matrix']['data']).reshape(3, 4)
+    data['width'] = intrinsics['image_width']
+    data['height'] = intrinsics['image_height']
+
     return data
