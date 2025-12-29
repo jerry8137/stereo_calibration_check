606f01a3899816ed6a6d5f89103d08e4cd1df489
Jerry Hou <jerry8137@gmail.com>
2025-12-29 11:31:20 -0500

    feat(calibrate): pass intrinsic instead of file path

--- calibrate/stereo_calibrate.py
+++ calibrate/stereo_calibrate.py
@@ -1,32 +1,30 @@
 import cv2
 import numpy as np
-from ..utils.file_utils import load_yaml
+from utils.file_utils import load_intrinsics
 
-
-def get_projection_matrix(calib_path1: str, calib_path2: str) -> tuple[np.ndarray, np.ndarray]:
+def get_projection_matrix(intrinsics1: dict,
+                          intrinsics2: dict) -> tuple[np.ndarray, np.ndarray]:
     """
     Get the projection matrices for stereo rectification from two calibration files.
     """
-    intrinsics1 = load_yaml(calib_path1)
-    intrinsics2 = load_yaml(calib_path2)
-    K1 = np.array(intrinsics1['camera_matrix']['data']).reshape(3, 3)
-    D1 = np.array(intrinsics1['distortion_coefficients']['data'])
-    # R1 = np.array(intrinsics1['rectification_matrix']['data']).reshape(3, 3)
-    P1 = np.array(intrinsics1['projection_matrix']['data']).reshape(3, 4)
-    K2 = np.array(intrinsics2['camera_matrix']['data']).reshape(3, 3)
-    D2 = np.array(intrinsics2['distortion_coefficients']['data'])
-    # R2 = np.array(intrinsics2['rectification_matrix']['data']).reshape(3, 3)
-    P2 = np.array(intrinsics2['projection_matrix']['data']).reshape(3, 4)
+    K1 = intrinsics1['K']
+    D1 = intrinsics1['D']
+    K2 = intrinsics2['K']
+    D2 = intrinsics2['D']
+    w = intrinsics1['width']
+    h = intrinsics1['height']
+
     R = np.eye(3)
-    T = np.array([[0], [0.12], [0]])
+    T = np.array([[0.12], [0], [0]])
     R1, R2, P1, P2, Q = cv2.fisheye.stereoRectify(
         K1,
         D1,
         K2,
-        D2, (640, 512),
+        D2, (w, h),
         R,
         T,
         flags=cv2.CALIB_ZERO_DISPARITY,
         balance=0.0)
-
     return P1, P2
+
+
--- calibrate/undistort.py
+++ calibrate/undistort.py
@@ -5,20 +5,19 @@ import cv2
 import numpy as np
 
 
-def rectify_image(image_path: str, output_path: [str | None], intrinsics: dict,
+def rectify_image(image: np.ndarray, output_path: [str | None], intrinsics: dict,
                   P_new: np.ndarray) -> np.ndarray:
     """
     Rectify a fisheye image using calibration data.
     """
-    cv_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    h, w = cv_image.shape[:2]
+    h, w = image.shape[:2]
     K = intrinsics['K']
     D = intrinsics['D']
     R = intrinsics['R']
 
     map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, R, P_new, (w, h),
                                                      cv2.CV_16SC2)
-    rectified_image = cv2.remap(cv_image, map1, map2, cv2.INTER_LINEAR)
+    rectified_image = cv2.remap(image, map1, map2, cv2.INTER_LINEAR)
 
     if output_path is not None:
         cv2.imwrite(output_path, rectified_image)
